ad3f0b85bc3223e9b128e594b5a14c0eb74b5fba
Austin Starin <austin.starin@gmail.com>
2017-11-06 06:17:26 -0500

    UPDATE_LIST now adds and removes from list depending on condition

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,10 +2,13 @@ import _ from 'lodash'
 
 export default {
   UPDATE_LIST (state, payload) {
-    const movie = _.find(state.movies, { 'id': payload.id })
+    const id = { 'id': payload.id }
     if (payload.list) {
+      const movie = _.find(state.list, id)
       movie.list = false
+      state.list = _.without(state.list, movie)
     } else {
+      const movie = _.find(state.movies, id)
       movie.list = true
       state.list.push(movie)
     }
