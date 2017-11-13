953fabfdeff4a6269f0f45ecd75125e5dd80ee94
bgantick <brian@punkave.com>
2017-11-13 16:29:57 -0500

    Add loading state to button

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,20 +1,24 @@
+// Fake asynchronous call to server
+function delay (t) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, t)
+  })
+}
+
 export default {
   COMMIT_ADD_TO_LIST ({ commit }, payload) {
-    // Fake asynchronous call to server
-    return setTimeout(() => {
+    return delay(500).then(() => {
       return commit('ADD_TO_LIST', payload)
-    }, 500)
+    })
   },
   COMMIT_REMOVE_FROM_LIST ({ commit }, payload) {
-    // Fake asynchronous call to server
-    return setTimeout(() => {
+    return delay(500).then(() => {
       return commit('REMOVE_FROM_LIST', payload)
-    }, 500)
+    })
   },
   COMMIT_UPDATE_RATING ({ commit }, payload) {
-    // Fake asynchronous call to server
-    return setTimeout(() => {
+    return delay(500).then(() => {
       return commit('UPDATE_RATING', payload)
-    }, 500)
+    })
   }
 }
