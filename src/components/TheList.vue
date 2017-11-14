bf12b4c4affa67ddd299c3af60c4069717fde8de
bgantick <brian@punkave.com>
2017-11-13 21:19:57 -0500

    Use MovieCard in list

--- src/components/TheList.vue
+++ src/components/TheList.vue
@@ -1,47 +1,36 @@
 <template>
   <section class="section">
     <div class="container">
-      <div v-if="movies.length > 0">
-        <div v-for="movie in movies">
-          <article class="columns media">
-            <figure class="column is-one-fifth media-left">
-              <img :src="movie.thumbnail" :alt="movie.alternative" />
-            </figure>
-            <div class="column is-half media-content">
-              <h2 class="title is-5">{{ movie.title }}</h2>
-              <p>{{ movie.synopsis }}</p>
-              <StarRating :id="movie.id" :rating="movie.rating"></StarRating>
-            </div>
-            <div class="column media-right">
-              <Update
-                :id="movie.id"
-                :list="movie.list"
-              />
-            </div>
-          </article>
-        </div>
+      <div class="columns is-multiline" v-if="movies.length > 0">
+        <MovieCard
+          v-for="movie in movies"
+          :key="movie.id"
+          :id="movie.id"
+          :title="movie.title"
+          :director="movie.director"
+          :thumbnail="movie.thumbnail"
+          :list="movie.list"
+          :rating="movie.rating"
+        >
+        </MovieCard>
       </div>
-      <div v-else>
-        <div class="notification is-info">
-          <p>
-            No movies in your list...
-            <router-link :to="{ name: 'index' }">Add More</router-link>
-          </p>
-        </div>
+      <div v-else class="notification is-info">
+        <p>
+          No movies in your list...
+          <router-link :to="{ name: 'index' }">Add More</router-link>
+        </p>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import StarRating from '@/components/StarRating'
-import Update from '@/components/Update'
+import MovieCard from '@/components/MovieCard'
 
 export default {
   name: 'TheList',
   components: {
-    StarRating,
-    Update
+    MovieCard
   },
   computed: {
     movies () {
